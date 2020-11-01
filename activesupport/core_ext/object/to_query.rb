92c21897a51dd7c19e9f7d94c92a659182b0c199
Timothy Rozycki <tim@Timothys-MBP.hawaii.rr.com>
2020-10-31 14:06:13 -1000

    Check incoming hash keys to decide whether to treat as an options hash. Additional tests.

--- activesupport/core_ext/object/to_query.rb
+++ activesupport/core_ext/object/to_query.rb
@@ -2,6 +2,8 @@ require 'active_support'
 require 'active_support/core_ext'
 
 class Hash
+  @@valid_options_for_to_query = Set[:namespace, :preserve_order].freeze
+
   # Backwards-compatible with ActiveSupport 5.2 and 6.0's version of this method.
   # Now that many of us expect hash ordering to be consistent and reliable,
   # the lexicographic sorting provided by the original ActiveSupport .to_hash() can be an inconvenience.
@@ -11,22 +13,22 @@ class Hash
   # For example: {breakfast: 'eggs', lunch: 'sandwich', dinner: 'steak', dessert: 'pie'}
   # returns "breakfast=eggs&dessert=pie&dinner=steak&lunch=sandwich" if the original .to_hash() method is used,
   # and "breakfast=eggs&lunch=sandwich&dinner=steak&dessert=pie" if .to_hash(preserve_order: true) is called instead.
-  def to_query(options = {})
-    if options.is_a?(Hash)
+  def to_query(options_or_namespace = nil)
+    if options_or_namespace.is_a?(Hash) && options_or_namespace.all? { |key, _value| @@valid_options_for_to_query.include?(key) }
       # support newer contract with an options hash
-      namespace = options[:namespace]
-      preserve_order = options[:preserve_order]
+      namespace = options_or_namespace[:namespace]
+      preserve_order = options_or_namespace[:preserve_order]
     else
-      # support original ActiveSupport contract with a single namespace argument
-      namespace = options
+      # support older contract with a single namespace argument
+      namespace = options_or_namespace
+      preserve_order = false
     end
     query = collect do |key, value|
       unless (value.is_a?(Hash) || value.is_a?(Array)) && value.empty?
         value.to_query(namespace ? "#{namespace}[#{key}]" : key)
       end
     end.compact
-
-    query.sort! unless namespace.to_s.include?("[]") || preserve_order
+    query.sort! unless preserve_order || namespace.to_s.include?("[]")
     query.join("&")
   end
 
--- activesupport/test/active_support/core_ext/to_query_test.rb
+++ activesupport/test/active_support/core_ext/to_query_test.rb
@@ -1,25 +1,54 @@
 class ToQueryTest < ActiveSupport::TestCase
 
   def test_hash_with_namespace_option
-    hash = { name: "Tim", nationality: "USA" }
-    assert_equal "user%5Bname%5D=Tim&user%5Bnationality%5D=USA", hash.to_query(namespace: "user")
+    hash = { name: "Nakshay", nationality: "Indian" }
+    assert_equal "user%5Bname%5D=Nakshay&user%5Bnationality%5D=Indian", hash.to_query(namespace: "user")
+  end
+
+  def test_hash_not_sorted_lexicographically_for_nested_structure
+    params = {
+      "foo" => {
+        "contents" => [
+          { "name" => "gorby", "id" => "123" },
+          { "name" => "puff", "d" => "true" }
+        ]
+      }
+    }
+    expected = "foo[contents][][name]=gorby&foo[contents][][id]=123&foo[contents][][name]=puff&foo[contents][][d]=true"
+    assert_equal expected, URI.decode_www_form_component(params.to_query)
   end
 
   def test_hash_not_sorted_lexicographically_if_option_specified
-    hash={type: "human", name: "Tim"}
-    assert_equal "type=human&name=Tim", hash.to_query(preserve_order: true)
+    hash = { type: "human", name: "Nakshay" }
+    assert_equal "type=human&name=Nakshay", hash.to_query(preserve_order: true)
   end
 
   def test_hash_sorted_lexicographically_if_option_specified
-    hash = { type: "human", name: "Tim" }
-    assert_equal "name=Tim&type=human", hash.to_query(preserve_order: false)
+    hash = { type: "human", name: "Nakshay" }
+    assert_equal "name=Nakshay&type=human", hash.to_query(preserve_order: false)
   end
 
-  def test_hash_with_all_options_provided
-    hash={type: "human", name: "Tim"}
-    expected = "earth[type]=human&earth[name]=Tim"
+  def test_hash_with_namespace_and_preserve_order_options_provided
+    hash = { type: "human", name: "Nakshay" }
+    expected = "earth[type]=human&earth[name]=Nakshay"
     actual = hash.to_query(preserve_order: true, namespace: "earth")
     assert_equal expected, URI.decode_www_form_component(actual)
+  end
+
+  def test_hash_as_a_namespace
+    hash = { type: "human", name: "Nakshay" }
+    hash_namespace = { weird_namespace: "yes" }
+    actual = hash.to_query(hash_namespace)
+    expected = '{:weird_namespace=>"yes"}[name]=Nakshay&{:weird_namespace=>"yes"}[type]=human'
+    assert_equal expected, CGI.unescape(actual)
+  end
+
+  def test_hash_with_hash_key
+    hash_as_key = { weird_key: "yes" }
+    main_hash = { hash_as_key => "myvalue" }
+    actual = main_hash.to_query()
+    expected = "weird_key%3Dyes=myvalue"
+    assert_equal expected, actual
   end
 
 end
